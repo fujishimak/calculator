4c6bf5ff7c3ed300f88d78dfc8cbb43ba9ec2185
Joshua Thomas <joshua.fujishima@gmail.com>
2017-05-03 20:28:01 -0500

    Added in logic for capturing user operations

--- js/calc.js
+++ js/calc.js
@@ -1,4 +1,5 @@
-var operations;
+var operations = [];
+var current_input;
 
 //Add
 function sum(a, b){
@@ -68,18 +69,64 @@ function getOperation(e){
 	return btn;
 }
 
+// For capturing operations in the form of a running string
 function addToOperations(op){
 
-	operations += op;	
+	operations.push(op);	
 }
 
-function bindResult(e) {
+//Separate the operations based on order and precedence (BOMDAS principle) and process until final input achieved
+function processOperations(){
+
+}
+
+
+// Capture numbers (and the decimal point) in a running string
+function captureNumbers(num){
+
+	current_input+=num;
+}
+
+function bindResultClickedEvent(e) {
 
 //Only invoked when = is pressed
 //Break up operations around +, -, /, x
 //Parse the substrings and run operations
 //Output final result
 
+}
+
+// When an operator is clicked the current_input should be captured, cleared and the operator added to the operations list
+function bindOperatorClickedEvent(e){
+
+	addToOperations(current_input);
+	current_input = "";
+	addToOperations(e.target.innerHTML);
+	//Set #output-main's innerHTML to 0
+
+
+}
+
+// When an operand is clicked, capture the number
+function bindOperandClickedEvent(e){
+
+	captureNumbers(e.target.innerHTML);
+
+}
+
+// Clear the input screen (reset to 0) and operations array
+function clearAll(){
+
+	operations = [];
+	//Set #output-record's innerHTML to an empty string
+
+}
+
+// Clear the last entered digit or operator and reset input screen to 0 (history maintained)
+function clearLast(){
+
+	operations.pop();
+	//Set #output-record's innerHTML to the new array (stringified)
 }
 
 $(document).ready(function(){
