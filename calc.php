120b60538d2df2abf7c64825c17dd113066622e5
Joshua Thomas <joshua.fujishima@gmail.com>
2017-05-16 09:30:27 -0500

    Added in sign toggling and result chaining

--- calc.php
+++ calc.php
@@ -10,6 +10,7 @@
 
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Orbitron" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="css/calc.css">
 
 </head>
@@ -20,47 +21,47 @@
 
 <div id="calc-elements" class="container-fluid">
 
-	<div id="banner">freeCodeCamp Calculator!</div>
+	<div id="banner"><strong>freeCodeCamp Calculator!</strong></div>
 
 <!-- Output div to show record or operations, actual input from the user and results -->
-	<div id="output" class="row">
+	<div id="output" class="row form-control">
 		<div id="output-record">0</div>
 		<div id="output-main">0</div>
 	</div>
 
 	<div class="row keypad">
-		<div class="col-md-3"><button class="btn btn-default">CE</button></div>
-		<div class="col-md-3"><button class="btn btn-default">C</button></div>
+		<div class="col-md-3"><button id="clearLast" class="btn btn-danger">CE</button></div>
+		<div class="col-md-3"><button id="clearAll" class="btn btn-danger">C</button></div>
 		<div class="col-md-3"><button class="btn btn-default operator">SQ</button></div>
-		<div class="col-md-3"><button id="divide" class="btn btn-default operator">/</button></div>
+		<div class="col-md-3"><button id="divide" class="btn btn-primary operator">/</button></div>
 	</div>
 
 	<div class="row keypad">
-		<div class="col-md-3"><button class="btn btn-default">7</button></div>
-		<div class="col-md-3"><button class="btn btn-default">8</button></div>
-		<div class="col-md-3"><button class="btn btn-default">9</button></div>
-		<div class="col-md-3"><button id="multiply" class="btn btn-default operator">X</button></div>		
+		<div class="col-md-3"><button class="btn btn-default operand">7</button></div>
+		<div class="col-md-3"><button class="btn btn-default operand">8</button></div>
+		<div class="col-md-3"><button class="btn btn-default operand">9</button></div>
+		<div class="col-md-3"><button id="multiply" class="btn btn-primary operator">X</button></div>		
 	</div>
 
 	<div class="row keypad">
-		<div class="col-md-3"><button class="btn btn-default">4</button></div>
-		<div class="col-md-3"><button class="btn btn-default">5</button></div>
-		<div class="col-md-3"><button class="btn btn-default">6</button></div>
-		<div class="col-md-3"><button id="subtract" class="btn btn-default operator">-</button></div>
+		<div class="col-md-3"><button class="btn btn-default operand">4</button></div>
+		<div class="col-md-3"><button class="btn btn-default operand">5</button></div>
+		<div class="col-md-3"><button class="btn btn-default operand">6</button></div>
+		<div class="col-md-3"><button id="subtract" class="btn btn-primary operator">-</button></div>
 	</div>
 
 	<div class="row keypad">
-		<div class="col-md-3"><button class="btn btn-default">1</button></div>
-		<div class="col-md-3"><button class="btn btn-default">2</button></div>
-		<div class="col-md-3"><button class="btn btn-default">3</button></div>
-		<div class="col-md-3"><button  id="add" class="btn btn-default operator">+</button></div>		
+		<div class="col-md-3"><button class="btn btn-default operand">1</button></div>
+		<div class="col-md-3"><button class="btn btn-default operand">2</button></div>
+		<div class="col-md-3"><button class="btn btn-default operand">3</button></div>
+		<div class="col-md-3"><button id="add" class="btn btn-primary operator">+</button></div>		
 	</div>
 
 	<div class="row keypad">
-		<div class="col-md-3"><button class="btn btn-default">+/-</button></div>
-		<div class="col-md-3"><button class="btn btn-default">0</button></div>
-		<div class="col-md-3"><button class="btn btn-default">.</button></div>
-		<div id="sum" class="col-md-3"><button class="btn btn-default operator">=</button></div>		
+		<div class="col-md-3"><button id="toggleSign" class="btn btn-primary">+/-</button></div>
+		<div class="col-md-3"><button class="btn btn-default operand">0</button></div>
+		<div class="col-md-3"><button id="decimal" class="btn btn-primary operand">.</button></div>
+		<div class="col-md-3"><button id="result" class="btn btn-success operator">=</button></div>		
 	</div>
 
 </div>
@@ -71,7 +72,6 @@
 <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script> -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-<script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
 <script src="js/calc.js"></script>
 </body>
 </html>
--- js/calc.js
+++ js/calc.js
@@ -1,10 +1,14 @@
 var operations = [];
-var current_input;
+var current_input = "";
+var resultClicked=0; //check if the result button was last clicked
+var operandClicked=0; //check if operand was last clicked
+var operatorClicked=0; //check if an operator was last clicked
+var result = 0; //Keep track of last result
 
 //Add
 function sum(a, b){
 
-	return a+b;
+	return parseFloat(a)+ parseFloat(b);
 }
 
 //Subtract
@@ -14,6 +18,7 @@ function sub(a, b){
 }
 
 //Divide
+//Remember to disallow div by 0
 function div(a, b){
 
 	return a/b;
@@ -27,46 +32,12 @@ function mult(a, b){
 
 function toggleSign(a){
 
-return -1 * a;
+return -1*a;
 }
 
-//Figure out what kind of key was clicked
+function printOperations(){
 
-// a = all the buttons a user clicked
-// a = "1+4 x 0"
-// console.log(a)
-function getOperation(e){
-
-	var btn = e.target.innerHTML;
-
-	// var op;
-
-	// switch(btn){
-
-	// 	case "0":
-	// 	case "1":
-	// 	case "2":
-	// 	case "3":
-	// 	case "4":
-	// 	case "5":
-	// 	case "6":
-	// 	case "7":
-	// 	case "8":
-	// 	case "9":
-	// 		return 
-	// 	case "+":
-	// 	case "-":
-	// 	case "/":
-	// 	case "x":
-	// 	case "=":
-	// 	case "SQ":
-	// 	case "C":
-	// 	case "CE":
-	// 	case ".":
-	// 	case "+/-":
-	// }
-
-	return btn;
+	return operations.join("");
 }
 
 // For capturing operations in the form of a running string
@@ -75,8 +46,45 @@ function addToOperations(op){
 	operations.push(op);	
 }
 
-//Separate the operations based on order and precedence (BOMDAS principle) and process until final input achieved
+//Separate the operations based on order and process until final input achieved
 function processOperations(){
+
+	addToOperations(current_input);
+	document.getElementById("output-record").innerHTML = printOperations();
+
+	var i;
+	result = operations[0];
+	//for each element, check if it's not '=', check i+2 then verify operation at i+1
+	for(i = 0; i < operations.length; i++){
+
+		if(operations[i+1] != "="){
+			console.log("Result is currently " + result);
+			//console.log("Next operator: " + operations[i+1] + " Next operand: " + operations[i+2]);
+			//var num1 = operations[i];
+			var num2 = operations[i+2];
+			switch (operations[i+1]){
+				
+				case "X":
+					result=mult(result, num2);
+					break;
+				case "/":
+					result=div(result, num2);
+					break;
+				case "+":
+					result=sum(result, num2);
+					break;
+				case "-":
+					result=sub(result, num2);
+					break;
+				
+			}
+			i+=1;
+		}
+
+		document.getElementById("output-main").innerHTML = result;
+	}
+
+
 
 }
 
@@ -88,21 +96,38 @@ function captureNumbers(num){
 }
 
 function bindResultClickedEvent(e) {
-
+	e.preventDefault();
 //Only invoked when = is pressed
 //Break up operations around +, -, /, x
 //Parse the substrings and run operations
 //Output final result
+	resultClicked = 1;
+	operatorClicked=0;
+	operandClicked=0;
+	processOperations();
+
 
 }
 
 // When an operator is clicked the current_input should be captured, cleared and the operator added to the operations list
 function bindOperatorClickedEvent(e){
 
+	e.preventDefault();
+	operatorClicked=1;
+	operandClicked=0;
+	//If the result is still in play, clear the current ops queue and set the current_input var to result
+	if(resultClicked){
+
+		operations = [];
+		current_input = result;
+		resultClicked = 0;
+	}	
 	addToOperations(current_input);
 	current_input = "";
 	addToOperations(e.target.innerHTML);
 	//Set #output-main's innerHTML to 0
+	document.getElementById("output-main").innerHTML = "0";
+	document.getElementById("output-record").innerHTML = printOperations();
 
 
 }
@@ -110,26 +135,96 @@ function bindOperatorClickedEvent(e){
 // When an operand is clicked, capture the number
 function bindOperandClickedEvent(e){
 
+	e.preventDefault();
+	operandClicked=1;
+	operatorClicked=0;
 	captureNumbers(e.target.innerHTML);
+	document.getElementById("output-main").innerHTML = current_input;
+	
+	if(printOperations()){
+		document.getElementById("output-record").innerHTML = printOperations() + current_input; // do not modify ops array until operator or result is clicked
+	} else {
+		document.getElementById("output-record").innerHTML = current_input;
+	}
+
+}
+
+//When the sign of an operand is changed, add the requisite sign to the front of current_input.
+function bindToggleSign(e){
+
+	e.preventDefault();
+
+	if(!resultClicked){
+		
+		if(operandClicked && (current_input.charAt(0) != "-")){
+			
+			current_input = "-" + current_input;
+		} else {
+
+			current_input = current_input.substr(1);
+		}
+
+		document.getElementById("output-main").innerHTML = current_input;
+		
+		if(printOperations()){
+			document.getElementById("output-record").innerHTML = printOperations() + current_input; // do not modify ops array until operator or result is clicked
+		} else {
+			document.getElementById("output-record").innerHTML = current_input;
+		}
+	}
 
 }
 
 // Clear the input screen (reset to 0) and operations array
-function clearAll(){
+function clearAll(e){
 
+	e.preventDefault();
 	operations = [];
+	result = 0;
+	resultClicked=0;
 	//Set #output-record's innerHTML to an empty string
-
+	document.getElementById("output-main").innerHTML = "0";
+	document.getElementById("output-record").innerHTML = "0";
 }
 
 // Clear the last entered digit or operator and reset input screen to 0 (history maintained)
-function clearLast(){
+function clearLast(e){
 
-	operations.pop();
+	e.preventDefault();
+	
 	//Set #output-record's innerHTML to the new array (stringified)
+	document.getElementById("output-main").innerHTML = "0";
+	
+	if(resultClicked){
+
+		clearAll(e);
+		
+	} else{
+
+		operations.pop();
+		current_input="";
+
+		if(printOperations()){
+			document.getElementById("output-record").innerHTML = printOperations(); // do not modify ops array until operator or result is clicked
+		} else {
+			document.getElementById("output-record").innerHTML = "0";
+		}
+	}
+}
+
+// Bind all click events
+function bindClickEvents(){
+
+	$(".operand").click(bindOperandClickedEvent);
+	$(".operator").click(bindOperatorClickedEvent);
+	$("#result").click(bindResultClickedEvent);
+	$("#clearLast").click(clearLast);
+	$("#clearAll").click(clearAll);
+	$("#toggleSign").click(bindToggleSign);
 }
 
 $(document).ready(function(){
 
+	bindClickEvents();
 
 });
